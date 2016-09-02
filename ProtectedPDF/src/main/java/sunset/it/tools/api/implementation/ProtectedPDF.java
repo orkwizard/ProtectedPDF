a6c3e5fdae84d9fbc93b386a3f81778cdfb8e399
edgarosorio <edgarosorio@192.168.0.101>
2016-09-02 07:42:22 -0500

    Ver. 0.0.2 https://www.mkyong.com/webservices/jax-rs/restful-java-client-with-apache-httpclient/

--- ProtectedPDF/src/main/java/sunset/it/tools/api/implementation/ProtectedPDF.java
+++ ProtectedPDF/src/main/java/sunset/it/tools/api/implementation/ProtectedPDF.java
@@ -1,5 +1,15 @@
 package sunset.it.tools.api.implementation;
 
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.params.HttpParams;
+
 import sunset.it.tools.pojo.ChallengeResponse;
 import sunset.it.tools.pojo.ResponseResponse;
 import sunset.it.tools.pojo.Session;
@@ -10,19 +20,40 @@ public class ProtectedPDF {
 	private String XVITRACCOUNTTOKEN;
 	private String XVITRSESSIONTOKEN;
 	
-	public ChallengeResponse loginChallenge(String context){
+	private  HttpClient httpClient;
+	
+	
+	public ChallengeResponse loginChallenge(String context){	
+		return null;
+	}
+	
+	public ResponseResponse loginResponse(String context){	
+		return null;
+	}
+	
+	public String authenticate(String usr,String pwd){
 		
-		
+		HttpPost post = new HttpPost(baseURL+"authenticate");
+		try {
+			StringEntity input = new StringEntity("{\"Username\":\"eosorio@sunset.com.mx \",\"Password\":\"test123\"}");
+			input.setContentType("application/json");
+			post.setEntity(input);
+			//https://www.mkyong.com/webservices/jax-rs/restful-java-client-with-apache-httpclient/
+			//HttpResponse response =  httpClien
+			
+			
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
 	
-	public ResponseResponse loginResponse(String context){
+	public ProtectedPDF(){
+		httpClient = HttpClientBuilder.create().build();
 		
-		
-		return null;
 	}
-	
 	
 	
 }
