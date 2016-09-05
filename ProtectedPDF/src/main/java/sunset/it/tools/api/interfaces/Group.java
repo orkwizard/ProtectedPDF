649c4ff67765a0a1cf0481373cc25e9be2aca176
edgarosorio <edgarosorio@192.168.0.101>
2016-09-04 19:41:07 -0500

    Ver. 0.0.4

--- ProtectedPDF/src/main/java/sunset/it/tools/api/interfaces/Group.java
+++ ProtectedPDF/src/main/java/sunset/it/tools/api/interfaces/Group.java
@@ -1,6 +1,17 @@
 package sunset.it.tools.api.interfaces;
 
+import java.util.ArrayList;
+
 public interface Group {
 
+	public Group getGroup(String Id);
+	public String deleteGroups(String[] groupUniqueIds);
+	public ArrayList<Group> getGroups(String page,String sort,String filter);
+	public String addGroup(sunset.it.tools.pojo.Group group);
+	public String deleteGroup(String Id);
+	public String updateGroup(sunset.it.tools.pojo.Group g,String id);
+	public String removeReaders(String groupId,ArrayList<String> readersId);
+	public String addReaders(String groupId,ArrayList<String> readersId);
+	
 	
 }
